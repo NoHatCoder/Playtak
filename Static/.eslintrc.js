a8bc0027304aa24b88f805010461c1048b5748a5
nitzel <jan.s.92@web.de>
2021-01-31 23:10:03 +0000

    Add ESLint for server.js #6

---
+++ Static/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    'max-len': ['error', 160, { ignoreTrailingComments: true }],
+    'linebreak-style': ['error', 'windows'],
+    'func-names': ['off'],
+    'object-shorthand': ['off'],
+    curly: ['off'],
+    'nonblock-statement-body-position': ['off'],
+    'no-unused-vars': ['off'],
+    'no-alert': ['off'],
+    'no-console': ['off'],
+    'prefer-destructuring': ['off'],
+    'brace-style': ['off'],
+  },
+  globals: {
+    output: 'readonly',
+    $: 'readonly',
+    board: 'readonly',
+    stopTime: 'readonly',
+    getZero: 'readonly',
+    chathandler: 'readonly',
+    botlist: 'readonly',
+    startTime: 'readonly',
+    lastWt: 'writeable',
+    lastBt: 'writeable',
+    lastTimeUpdate: 'writeable',
+  },
+};
--- Static/README.md
+++ Static/README.md
@@ -1,4 +1,14 @@
 # TakWeb
 Javascript based Tak client
-<br>
+
 If you want to test your changes with TakServer running locally on your machine, you'll need websockify (https://github.com/novnc/websockify)
+
+
+### Dev
+#### ESLint for code styling
+We are currently in the progress of overhauling the code base code style for better maintainability.
+##### To utilize ESLint
+  - Use vs-code
+  - Install an ESLint extension (e.g. `dbaeumer.vscode-eslint`)
+  - Install nodejs
+  - run `npm install` in `/Static`
